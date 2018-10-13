3ac7b66968ed4ad96104e27f9c17cd4de7c73d0d
David A. Knowles <knowles84@gmail.com>
2018-10-13 14:04:52 -0700

    can fix concentration parameter

--- eagle2/R/eagle2.R
+++ eagle2/R/eagle2.R
@@ -1,5 +1,5 @@
 #' Robust inverse of PSD matrix using SVD
-#' 
+#'
 #' @param si Matrix to invert
 #' @param eigenThreshold Threshold eigenvalues smaller than this
 #' @return Inverted matrix
@@ -9,8 +9,8 @@ robustSolve=function(si, eigenThreshold=0.01){
   svdSigma$vectors %*% diag(1/svdSigma$values) %*% t(svdSigma$vectors)
 }
 
-#' Function to extract coefficients, standard errors, and Wald p-values. 
-#' 
+#' Function to extract coefficients, standard errors, and Wald p-values.
+#'
 #' @param fit Fitted object returned from rstan::optimizing. Must have $par$beta and $hessian available
 #' @return Data.frame with coefs, standard errors, Wald p-values
 #' @export
@@ -24,96 +24,96 @@ get_coefs=function(fit) {
   data.frame(coef=fit$par$beta,se=betase,wald_p=2.0*pnorm(-abs(zscore)))
 }
 
-#' Detect potential homozygote (or imprinted) SNP-individual pairs and zero them out. 
-#' 
+#' Detect potential homozygote (or imprinted) SNP-individual pairs and zero them out.
+#'
 #' @param a Numerator counts [n x T x K] where n are individuals, T are timepoints, K are SNPs
 #' @param nh denominator counts [n x T x K]
 #' @param concShape Shape of prior on concentration
 #' @param concRate Rate of prior on concentration
 #' @param errorRate Assumed probability of erroneously observing a read with the alternative allele, due to sequencing or alignment error
-#' @param posterior_threshold If posterior probability of being a het is below this the individual/SNP pair will be ignored. 
-#' @return Filtered a and nh matrices, and which SNPs were kept. 
+#' @param posterior_threshold If posterior probability of being a het is below this the individual/SNP pair will be ignored.
+#' @return Filtered a and nh matrices, and which SNPs were kept.
 #' @import foreach
 #' @importFrom rstan optimizing
 #' @export
 detect_homozygotes=function(a,nh,concShape=1.0001,concRate=1e-4,errorRate=0.01,posterior_theshold=0.95,verbose=T)
 {
-  
+
   homo=foreach(snp_index=seq_len(dim(nh)[3]), .combine=cbind) %do% {
     as=a[,,snp_index]
     nhh=nh[,,snp_index]
     ind_to_keep=rowSums(nhh)>0
     treat_to_keep=colSums(nhh)>0
-    
+
     as=as[ind_to_keep,treat_to_keep,drop=F]
     nhh=nhh[ind_to_keep,treat_to_keep,drop=F]
-    
+
     o=optimizing(stanmodels$is_het, dat=list(N=nrow(nhh), T=ncol(nhh), errorRate=errorRate, concShape=concShape, concRate=concRate, ys=as, ns=nhh), as_vector=F)
     eo=exp(o$par$probs)
     pr=sweep(eo, 1, rowSums(eo), "/")
-    
+
     homo=pr[,1]<posterior_theshold
-    
+
     if (verbose) cat("Removing",sum(homo),"individual(s) from SNP",dimnames(a)[[3]][snp_index],"\n")
-    
+
     nh[which(ind_to_keep)[homo],treat_to_keep,snp_index]=0
     a[which(ind_to_keep)[homo],treat_to_keep,snp_index]=0
   }
-  
+
   snp_to_keep=apply(nh>0, 3, any)
   ind_to_keep=apply(nh,1,sum) > 0
   if (sum(snp_to_keep)==0 | sum(ind_to_keep)==0) return(NULL)
   a=a[ind_to_keep,,snp_to_keep,drop=F]
   nh=nh[ind_to_keep,,snp_to_keep,drop=F]
-  
+
   list(a=a,nh=nh,snp_to_keep=snp_to_keep)
 }
 
-#' Detect potential homozygote (or imprinted) SNP-individual pairs and zero them out. 
-#' 
+#' Detect potential homozygote (or imprinted) SNP-individual pairs and zero them out.
+#'
 #' @param a Numerator counts [n x T x K] where n are individuals, T are timepoints, K are SNPs
 #' @param nh denominator counts [n x T x K]
 #' @param concShape Shape of prior on concentration
 #' @param concRate Rate of prior on concentration
 #' @param errorRate Assumed probability of erroneously observing a read with the alternative allele, due to sequencing or alignment error
-#' @param posterior_threshold If posterior probability of being a het is below this the individual/SNP pair will be ignored. 
-#' @return Filtered a and nh matrices, and which SNPs were kept. 
+#' @param posterior_threshold If posterior probability of being a het is below this the individual/SNP pair will be ignored.
+#' @return Filtered a and nh matrices, and which SNPs were kept.
 #' @import foreach
 #' @importFrom rstan optimizing
 #' @export
 detect_homozygotes_w_replicatess=function(a,nh,concShape=1.0001,concRate=1e-4,errorRate=0.01,posterior_theshold=0.95,verbose=T)
 {
-  
+
   homo=foreach(snp_index=seq_len(dim(nh)[3]), .combine=cbind) %do% {
     as=a[,,snp_index,]
     as=foreach(i=seq_len(dim(as)[3]), .combine = cbind)  %do% { as[,,i] }
     nhh=nh[,,snp_index,]
     nhh=foreach(i=seq_len(dim(nhh)[3]), .combine = cbind)  %do% { nhh[,,i] }
-    
+
     ind_to_keep=rowSums(nhh)>0
     treat_to_keep=colSums(nhh)>0
-    
+
     as=as[ind_to_keep,treat_to_keep,drop=F]
     nhh=nhh[ind_to_keep,treat_to_keep,drop=F]
-    
+
     o=optimizing(stanmodels$is_het, dat=list(N=nrow(nhh), T=ncol(nhh), errorRate=errorRate, concShape=concShape, concRate=concRate, ys=as, ns=nhh), as_vector=F)
     eo=exp(o$par$probs)
     pr=sweep(eo, 1, rowSums(eo), "/")
-    
+
     homo=pr[,1]<posterior_theshold
-    
+
     if (verbose) cat("Removing",sum(homo),"individual(s) from SNP",dimnames(a)[[3]][snp_index],"\n")
-    
+
     nh[which(ind_to_keep)[homo],,snp_index,]=0
     a[which(ind_to_keep)[homo],,snp_index,]=0
   }
-  
+
   snp_to_keep=apply(nh>0, 3, any)
   ind_to_keep=apply(nh,1,sum) > 0
   if (sum(snp_to_keep)==0 | sum(ind_to_keep)==0) return(NULL)
   a=a[ind_to_keep,,snp_to_keep,,drop=F]
   nh=nh[ind_to_keep,,snp_to_keep,,drop=F]
-  
+
   list(a=a,nh=nh,snp_to_keep=snp_to_keep)
 }
 
@@ -121,6 +121,7 @@ detect_homozygotes_w_replicatess=function(a,nh,concShape=1.0001,concRate=1e-4,er
 #'
 #' @param ys numerator counts [n x T x K] where n are individuals, T are timepoints, K are SNPs
 #' @param ns denominator counts [n x T x K]
+#' @param fixed_conc Optional fixed K-vector of concentration parameters
 #' @param concShape Shape of prior on concentration
 #' @param concRate Rate of prior on concentration
 #' @return List with likelihood ratio, p-value and fits
@@ -128,34 +129,38 @@ detect_homozygotes_w_replicatess=function(a,nh,concShape=1.0001,concRate=1e-4,er
 #' @importFrom foreach foreach %do%
 #' @importFrom abind abind
 #' @export
-eagle2=function(ys,ns,concShape=1.0001,concRate=1e-4,hessian=T,...) {
-  
+eagle2=function(ys,ns,fixed_conc=NULL,concShape=1.0001,concRate=1e-4,hessian=T,...) {
+
   N=dim(ys)[1]
   Ti=dim(ys)[2]
   K=dim(ys)[3]
-  
+
   preprocess_x=function(g) aperm( abind(g,along=3), c(3,1,2) )
-  
+
+  stan_model_to_use=if (is.null(fixed_conc)) stanmodels$bb else stanmodels$bb_fixed_conc
+
   xNull=preprocess_x( foreach(i=seq_len(Ti)) %do% matrix(1,N,1) )
-  dat=list(N=N,P=1,T=Ti,K=K,ys=ys,ns=ns,x=xNull,concShape=concShape,concRate=concRate)
+  dat=list(N=N,P=1,T=Ti,K=K,ys=ys,ns=ns,x=xNull,
+           concShape=concShape,concRate=concRate,
+           conc=if(is.null(fixed_conc)) NULL else as.array(fixed_conc))
 
   # Fit null model
-  fit_null <- optimizing(stanmodels$bb, data=dat, as_vector=F, hessian=T, ...)
-  
+  fit_null <- optimizing(stan_model_to_use, data=dat, as_vector=F, hessian=T, ...)
+
   # Initialize alternative model using null model
   initFull=fit_null$par
   initFull$beta=c(fit_null$par$beta,numeric(Ti-1))
-  
+
   # Fit alternative model
   datFull=dat
   temp=matrix(0,N,Ti)
   temp[,1]=1
   datFull$x=preprocess_x(foreach(i=seq_len(Ti)) %do% { temp[,i]=1; temp })
   datFull$P=Ti
-  fit_full <- optimizing(stanmodels$bb, data=datFull, init=initFull, as_vector=F, hessian=T, ...)
-  
+  fit_full <- optimizing(stan_model_to_use, data=datFull, init=initFull, as_vector=F, hessian=T, ...)
+
   loglr=fit_full$value - fit_null$value
-  
+
   list( loglr=loglr, lrtp=pchisq( 2.0*loglr, lower.tail = F , df=Ti-1 ), fit_full=fit_full, fit_null=fit_null )
 }
 
@@ -171,24 +176,24 @@ eagle2=function(ys,ns,concShape=1.0001,concRate=1e-4,hessian=T,...) {
 #' @importFrom abind abind
 #' @export
 eagle2_w_replicates=function(ys,ns,concShape=1.0001,concRate=1e-4,hessian=T,...) {
-  
+
   N=dim(ys)[1]
   Ti=dim(ys)[2]
   K=dim(ys)[3]
   R=dim(ys)[4]
-  
+
   preprocess_x=function(g) aperm( abind(g,along=3), c(3,1,2) )
-  
+
   xNull=preprocess_x( foreach(i=seq_len(Ti)) %do% matrix(1,N,1) )
   dat=list(N=N,P=1,T=Ti,K=K,R=R,ys=ys,ns=ns,x=xNull,concShape=concShape,concRate=concRate)
-  
+
   # Fit null model
   fit_null <- optimizing(stanmodels$bb_w_replicates, data=dat, as_vector=F, hessian=T, ...)
-  
+
   # Initialize alternative model using null model
   initFull=fit_null$par
   initFull$beta=c(fit_null$par$beta,numeric(Ti-1))
-  
+
   # Fit alternative model
   datFull=dat
   temp=matrix(0,N,Ti)
@@ -196,9 +201,9 @@ eagle2_w_replicates=function(ys,ns,concShape=1.0001,concRate=1e-4,hessian=T,...)
   datFull$x=preprocess_x(foreach(i=seq_len(Ti)) %do% { temp[,i]=1; temp })
   datFull$P=Ti
   fit_full <- optimizing(stanmodels$bb, data=datFull, init=initFull, as_vector=F, hessian=T, ...)
-  
+
   loglr=fit_full$value - fit_null$value
-  
+
   list( loglr=loglr, lrtp=pchisq( 2.0*loglr, lower.tail = F , df=Ti-1 ), fit_full=fit_full, fit_null=fit_null )
 }
 
---
+++ eagle2/src/stan_files/bb_fixed_conc.stan
@@ -0,0 +1,45 @@
+// EAGLE extension for repeated measurements of the same individual, in T conditions. 
+// BB(n,a,sigma(phi*beta*x)) is the model, where phi in (-1,1) corresponding to the phase. 
+// This version allows for K SNPs
+// No constraints on cofficients.
+functions {
+  // would be more efficient to pre-calc p
+  real beta_binomial_reparam_lpmf(int y, int n, real g, real conc) {
+    real p; 
+    p = inv_logit(g);
+    return beta_binomial_lpmf(y | n, conc*p, conc*(1.0-p));
+  }
+}
+data {
+  int<lower=0> N; // individuals
+  int<lower=0> P; // covariates
+  int<lower=0> K; // SNPs
+  int<lower=0> T; // time points
+  matrix[N,P] x[T]; // covariates
+  int<lower=0> ys[N,T,K]; // minor allele counts
+  int<lower=0> ns[N,T,K]; // total coverage counts
+  real<lower=0> concShape; // gamma prior on concentration parameters
+  real<lower=0> concRate;
+  real<lower=0> conc[K]; // per exonic SNP concentration parameters
+}
+parameters {
+  vector[P] beta; // effect sizes
+  real<lower=0,upper=1> p[K]; // learnt prior on phase (per SNP)
+}
+model {
+  vector[N] xb[T]; 
+  for (t in 1:T)
+    xb[t] = x[t] * beta;
+  for (n in 1:N) {
+    for (k in 1:K) {
+      vector[T] log_prob_unflipped;
+      vector[T] log_prob_flipped; 
+      for (t in 1:T) {
+        log_prob_unflipped[t] = beta_binomial_reparam_lpmf(ys[n,t,k] | ns[n,t,k], xb[t][n], conc[k]); 
+        log_prob_flipped[t] = beta_binomial_reparam_lpmf(ys[n,t,k] | ns[n,t,k], -xb[t][n], conc[k]);
+      }
+      target += log_sum_exp( log(p[k]) + sum(log_prob_unflipped), log(1.0-p[k]) + sum(log_prob_flipped) ) ;
+    }
+  }
+  conc ~ gamma(concShape, concRate);
+}
